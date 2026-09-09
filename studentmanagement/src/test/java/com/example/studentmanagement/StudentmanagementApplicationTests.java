5439d62e9700a0090cbf4129f0c4c762d4919313
Atif <atifnagorwala8283@gmail.com>
2026-09-09 17:29:16 +0530

    Fix Maven test configuration

--- studentmanagement/src/test/java/com/example/studentmanagement/StudentmanagementApplicationTests.java
+++ studentmanagement/src/test/java/com/example/studentmanagement/StudentmanagementApplicationTests.java
@@ -2,8 +2,12 @@ package com.example.studentmanagement;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+		"AI_API_KEY=test-key"
+})
 class StudentmanagementApplicationTests {
 
 	@Test
